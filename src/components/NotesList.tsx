1393e0677fc75f78de13178924cca85d7baf8a28
Jameem <jameemkurikkal.mp@gmail.com>
2021-12-11 18:25:46 +0100

    feat: add note list component

---
+++ src/components/NotesList.tsx
@@ -0,0 +1,25 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import noteStore from "../NoteStore";
+
+interface NotesProps{
+
+}
+
+const NotesList: React.FC<NotesProps> = (props)=>{
+
+    return <div>
+        <h3>List</h3>
+
+        {
+            noteStore.notes.map((note, index)=>{
+                return <>
+                <h5>{index+1}. {note} <small style={{cursor: "pointer"}} onClick={()=>noteStore.removeNote(note)}>Remove</small></h5>
+                
+                </>
+            })
+        }
+    </div>
+}
+
+export default observer(NotesList)
