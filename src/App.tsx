36f0880cf9bf78460ce3c9bfed442af794c71106
Jameem <jameemkurikkal.mp@gmail.com>
2021-12-13 08:54:44 +0100

    feat: wire up the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NotesList from './components/NotesList';
+import NotesAdd from './components/NotesAdd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NotesAdd/>
+      <NotesList/>
     </div>
   );
 }
