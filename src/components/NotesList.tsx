f0070ab89484e8deb7f91933f1a5714de544fc18
Jameem <jameemkurikkal.mp@gmail.com>
2022-02-09 10:41:37 +0100

    feat: test step4

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -2,24 +2,32 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import noteStore from "../NoteStore";
 
-interface NotesProps{
+interface NotesProps {}
 
-}
+//STEP4: This is to test merging
 
-const NotesList: React.FC<NotesProps> = (props)=>{
+const NotesList: React.FC<NotesProps> = (props) => {
+  return (
+    <div>
+      <h3>List</h3>
 
-    return <div>
-        <h3>List</h3>
-
-        {
-            noteStore.notes.map((note, index)=>{
-                return <>
-                <h5>{index+1}. {note} <small style={{cursor: "pointer"}} onClick={()=>noteStore.removeNote(note)}>Remove</small></h5>
-                
-                </>
-            })
-        }
+      {noteStore.notes.map((note, index) => {
+        return (
+          <>
+            <h5>
+              {index + 1}. {note}{" "}
+              <small
+                style={{ cursor: "pointer" }}
+                onClick={() => noteStore.removeNote(note)}
+              >
+                Remove
+              </small>
+            </h5>
+          </>
+        );
+      })}
     </div>
-}
+  );
+};
 
-export default observer(NotesList)
+export default observer(NotesList);
